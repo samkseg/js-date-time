26e3cc7f0bc12814088285a321dedc6028fd60a5
Ken <ken.segawa@iths.se>
2023-09-12 01:45:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let theTime = document.createElement("p");
+let time = new Date();
+
+function loadTime() {
+    let currentDate = new Date();
+
+    let secs = pad(currentDate.getSeconds());
+    let mins = pad(currentDate.getMinutes());
+    let hrs = pad(currentDate.getHours());
+
+    let dateTime = "Idag är det den " + currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + ". Året är " + currentDate.getFullYear() + ". Klockan är " + hrs + ":" + mins + ":" + secs;
+    
+    theTime.innerHTML = dateTime;
+    document.getElementById("dateTimeDisplay").appendChild(theTime);
+
+    function pad(unit) {
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
+    }
+}
+loadTime();
+setInterval(loadTime, 1000);
